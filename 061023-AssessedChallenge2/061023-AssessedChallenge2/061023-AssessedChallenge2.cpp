54ce300b054db11f2b5b8aa5cd9c6155cf868ddb
Izzet <jamesliamneville@gmail.com>
2023-10-06 21:07:23 +0100

    Assessed Challenge 2

---
+++ 061023-AssessedChallenge2/061023-AssessedChallenge2/061023-AssessedChallenge2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void inputs() {
+    string name;
+    string clanTag;
+    string username;
+    int age;
+
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << "Please enter your clan tag: ";
+    cin >> clanTag;
+    cout << "Please enter your username: ";
+    cin >> username;
+    cout << "Please enter your age: ";
+    cin >> age;
+
+    cout << "\nPlayer details:\n";
+    cout << "Name: [" << clanTag << "]" << username << "\n";
+    cout << "Age: " << age << "\n";
+}
+
+int main() {
+    inputs();
+}
