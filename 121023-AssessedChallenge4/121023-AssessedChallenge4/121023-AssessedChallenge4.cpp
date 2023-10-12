17e04bae76062533846d169085d0ead62b94f797
Izzet <jamesliamneville@gmail.com>
2023-10-12 10:38:56 +0100

    Challenge 4 - unfinished

---
+++ 121023-AssessedChallenge4/121023-AssessedChallenge4/121023-AssessedChallenge4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string boxBoundIteration(string userInput) {
+    string output = "";
+    for (int i = 0; i < userInput.length(); i++) {
+        output += "*";
+    }
+    output += "****";
+    return output;
+}
+
+void makeBox(string userInput) {
+    cout << boxBoundIteration(userInput);
+    cout << "\n" << "* " << userInput << " *" << "\n";
+    cout << boxBoundIteration(userInput);
+}
+
+int main() {
+    string userInput = "";
+
+    cout << "Please enter some text: ";
+    cin >> userInput;
+    makeBox(userInput);
+}
